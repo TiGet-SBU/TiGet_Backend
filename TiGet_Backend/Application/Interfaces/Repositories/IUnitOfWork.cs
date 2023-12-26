f9a4ae0e6bdb0b80ede19b20e70914fcaa093634
Yazdan Jahedi <yzdn.jt@gmail.com>
2023-12-26 17:34:07 +0330

    Add unitOfWork

---
+++ TiGet_Backend/Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public Task SaveAsync();
+        public Task<IDbContextTransaction> BeginTransactionAsync();
+
+
+        // repositories
+        public ICityRepositoty CityRepositoty { get; }
+        public ICompanyRepository CompanyRepository { get; }
+        public ICustomerRepository CustomerRepository { get; }
+        public IOrderRepository OrderRepository { get; }
+        public IStationRepository StationRepository { get; }
+        public ITicketRespsitory TicketRespsitory { get; }
+        public IVehicleRepository VehicleRepository { get; }
+    }
+}
