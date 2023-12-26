9c1ad9f20931139b4e68f8bcae06dc11b228ee8a
Yazdan Jahedi <yzdn.jt@gmail.com>
2023-12-26 23:15:47 +0330

    Add TicketDTOs

---
+++ TiGet_Backend/Application/DTOs/TicketDTO/TicketGetAllRequest.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TicketDTO
+{
+    public record TicketGetAllRequest
+    {
+        public int first = 0;
+        public int last = int.MaxValue;
+        public required Expression<Func<Ticket, bool>>? condition = null;
+    }
+}
---
+++ TiGet_Backend/Application/DTOs/TicketDTO/TicketGetResponse.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TicketDTO
+{
+    public record TicketGetResponse
+    {
+        public required DateTime TimeToGo { get; set; }
+        public required double Price { get; set; }
+        public required Vehicle Vehicle { get; set; }
+        public required Company Company { get; set; }
+        public required Station Source { get; set; }
+        public required Station Destination { get; set; }
+    }
+}
