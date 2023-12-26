24042410f91d24588435a67380a60a6cf931073a
Yazdan Jahedi <yzdn.jt@gmail.com>
2023-12-26 23:56:43 +0330

    Split Auth semantinc and Customer serices

---
+++ TiGet_Backend/Infrastructure/Services/AuthService.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class AuthService
+    {
+        private readonly string _jwtSecret = "";
+        private readonly static int _jwtExpirationInMinutes = 3600; // Token expiration time
+        private static string GenerateRandomKey(int lengthInBits)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] keyBytes = new byte[lengthInBits / 8];
+                rng.GetBytes(keyBytes);
+                return Convert.ToBase64String(keyBytes);
+            }
+        }
+        public static string GenerateJwtToken(User user)
+        {
+            var _jwtSecret = GenerateRandomKey(256);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSecret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            }),
+                Expires = DateTime.UtcNow.AddMinutes(_jwtExpirationInMinutes),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public static string HashPassword(string password)
+        {
+            // Hash the password using BCrypt
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string enteredPassword, string storedPasswordHash)
+        {
+            // Verify the password using BCrypt
+            return BCrypt.Net.BCrypt.Verify(enteredPassword, storedPasswordHash);
+        }
+    }
+}
--- TiGet_Backend/Infrastructure/Services/CustomerService.cs
+++ TiGet_Backend/Infrastructure/Services/CustomerService.cs
@@ -1,38 +1,18 @@
-﻿using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using BCrypt.Net;
-using Application.DTOs;
-using Application.DTOs.CustomerDTO.Auth;
+﻿using Application.DTOs.CustomerDTO.Auth;
 using Application.DTOs.TicketDTO;
 using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
 using Domain.Entities;
 using Domain.Enums;
-using Microsoft.IdentityModel.Tokens;
-using System.Security.Cryptography;
+using Infrastructure.Services;
 
 public class CustomerService : ICustomerService
 {
     private readonly IUnitOfWork _unitOfWork;
-    private readonly string _jwtSecret = "";
-    private readonly int _jwtExpirationInMinutes = 3600; // Token expiration time
 
     public CustomerService(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
-    }
-
-    private string GenerateRandomKey(int lengthInBits)
-    {
-        using (var rng = new RNGCryptoServiceProvider())
-        {
-            byte[] keyBytes = new byte[lengthInBits / 8];
-            rng.GetBytes(keyBytes);
-            return Convert.ToBase64String(keyBytes);
-        }
     }
 
 
@@ -55,7 +35,7 @@ public class CustomerService : ICustomerService
             Id = Guid.NewGuid(),
             Role = Role.Customer,
             Email = req.Email,
-            PasswordHash = HashPassword(req.Password),
+            PasswordHash = AuthService.HashPassword(req.Password),
             CreatedDate = DateTime.Now,
         };
 
@@ -86,10 +66,10 @@ public class CustomerService : ICustomerService
         var user = await _unitOfWork.CustomerRepository.GetByConditionAsync(e => e.Email == req.Email);
 
         // Check if the user exists and the password is correct
-        if (user != null && VerifyPassword(req.Password, user.PasswordHash))
+        if (user != null && AuthService.VerifyPassword(req.Password, user.PasswordHash))
         {
             // Return a JWT token
-            string token =  GenerateJwtToken(user);
+            string token =  AuthService.GenerateJwtToken(user);
             var response = new CustomerLoginResponse
             {
                 Email = user.Email,
@@ -120,41 +100,6 @@ public class CustomerService : ICustomerService
         });
 
         return response;
-    }
-
-    // ----------
-    private string GenerateJwtToken(User user)
-    {
-        var _jwtSecret = GenerateRandomKey(256);
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_jwtSecret);
-
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, user.Id.ToString()),
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.Role, user.Role.ToString())
-            }),
-            Expires = DateTime.UtcNow.AddMinutes(_jwtExpirationInMinutes),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-        };
-
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        return tokenHandler.WriteToken(token);
-    }
-
-    private string HashPassword(string password)
-    {
-        // Hash the password using BCrypt
-        return BCrypt.Net.BCrypt.HashPassword(password);
-    }
-
-    private bool VerifyPassword(string enteredPassword, string storedPasswordHash)
-    {
-        // Verify the password using BCrypt
-        return BCrypt.Net.BCrypt.Verify(enteredPassword, storedPasswordHash);
     }
 
 }
