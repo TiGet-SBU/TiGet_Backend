e602b0fc7f3bc76cc770d3b03564c1e7d4a98895
zahra mokhtari <mokhtariii.zahraaa@gmail.com>
2024-01-28 16:03:22 +0330

    order controller adde

--- TiGet_Backend/Presentation/Controllers/OrderController.cs
+++ TiGet_Backend/Presentation/Controllers/OrderController.cs
@@ -1,6 +1,34 @@
-﻿namespace Presentation.Controllers
+﻿using Application.DTOs.OrderDTO;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
 {
-    public class OrderController
+
+    [Route("api/order")] // Adjusted route for clarity
+    [ApiController]
+    public class OrderController : ControllerBase
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddOrder([FromBody] AddOrderRequest request)
+        {
+            try
+            {
+                var response = await _orderService.AddOrder(request);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
